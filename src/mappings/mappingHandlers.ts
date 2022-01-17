2792962c91e4b445a462a68209b09dcc713f4664
Vladyslav Naprykovskyi <naprikovsky@gmail.com>
2022-01-17 21:20:18 +0200

    Module 4

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,13 +1,13 @@
-import { SubstrateEvent } from "@subql/types";
-import { StakingReward, SumReward } from "../types";
-import { Balance } from "@polkadot/types/interfaces";
+import {SubstrateEvent} from "@subql/types";
+import {StakingReward, SumReward} from "../types";
+import {Balance} from "@polkadot/types/interfaces";
 
-export async function handleStakingRewarded(event: SubstrateEvent):
-    Promise<void> {
-    const { event: { data: [account, newReward] } } = event;
+export async function handleStakingReward(event: SubstrateEvent):
+Promise<void> {
+    const {event: {data: [account, newReward]}} = event;
     const entity = new
-        StakingReward(`${event.block.block.header.number}-${event.idx.toString()
-            }`);
+StakingReward(`${event.block.block.header.number}-${event.idx.toString()
+}`);
     entity.accountId = account.toString();
     entity.balance = (newReward as Balance).toBigInt();
     entity.date = event.block.timestamp;
@@ -19,13 +19,13 @@ function createSumReward(accountId: string): SumReward {
     entity.totalReward = BigInt(0);
     return entity;
 }
-export async function handleSumRewarded(event: SubstrateEvent):
-    Promise<void> {
-    const { event: { data: [account, newReward] } } = event;
+export async function handleSumReward(event: SubstrateEvent):
+Promise<void> {
+    const {event: {data: [account, newReward]}} = event;
     let entity = await SumReward.get(account.toString());
-    if (entity === undefined) {
-        entity = createSumReward(account.toString());
-    }
+    if (entity === undefined){
+    entity = createSumReward(account.toString());
+}
     entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt();
     entity.blockheight = event.block.block.header.number.toNumber();
     await entity.save();
